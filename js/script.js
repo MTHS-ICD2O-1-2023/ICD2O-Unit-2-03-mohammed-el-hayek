dcfb677138a0bfd398d32b4cf4f31ca80b6526c0
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-03-21 02:47:14 +0000

    finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed El-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed El-hayek
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const userhousenumber = parseInt(document.getElementById("House").value)
+  const userStreetname = document.getElementById("Street").value
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + userhousenumber + userStreetname + "."
+}
